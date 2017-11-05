85d4bff4a6cb82a6e085919924bdf9463edc7701
Rafael Goncalves <3rd.rafael.goncalves@almundo.com>
2017-11-04 22:25:57 -0200

    removing cors that was blocking any requests

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,28 +4,27 @@ const queryParser = require('express-query-int')
 
 const express = require('express')
 const app = express()
-var server = require('http').createServer(app)
 const cors = require('cors')
 const http = require('http')
 
-const io = require('socket.io')(server)
+
 const socketConfig = require('./socketConfig')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors)
+// app.use(cors) -- TODO: OLHAR PRA VER O QUE ESTÁ BARRANDO
 app.use(queryParser())
 
 //io config
-// var _http = http.Server(app);
-// var _io = new io(_http, { origins: '*:*'});
-socketConfig(io)
 
-server.listen(port, function() {
+const server = app.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`)
 })
 
-module.exports = {server, express}
+const io = require('socket.io')(server)
+socketConfig(io)
+
+module.exports = app
 
 
 // const allowCors = require('./cors')
